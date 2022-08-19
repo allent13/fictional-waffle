dab9c1d04ad68e1a8cfa3ee33757a2d3bb458915
allent13 <allentang113@gmail.com>
2022-08-19 16:59:31 -0700

    adds movement handler for char

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ function renderChar(){
 
 renderChar()
 
-// create enemy class
+// create enemy class. do i need a bullet class?
 class Enemy {
     constructor(x, y, width, height, color) {
         this.x = x
@@ -41,10 +41,40 @@ class Enemy {
     }
 }
 
-
-
 // player movement + shooting handler
+document.addEventListener('keypress', e => {
+    const speed = 10
+    if (char.alive){
+        switch (e.key) {
+            case('w'):
+                char.y -= speed
+                if (char.y < 0) {
+                    char.y = 0
+                }
+                break
+            case('s'):
+                char.y += speed
+                if (char.y + char.height > canvas.height) {
+                    char.y = canvas.height - char.height
+                }
+                break
+            case('a'):
+                char.x -= speed
+                if (char.x < 0) {
+                    char.x = 0
+                }
+                break
+            case('d'):
+                char.x += speed
+                if (char.x + char.width > canvas.width) {
+                    char.x = canvas.width - char.width
+                }
+                break
+            case(' '):
 
+                break
+            }}
+        })
 // bullet render and movement
 
 // enemy render/placement
